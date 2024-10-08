0ec5ac6b773d991578e5f5adf362f1aed26efb2c
Foziljon <qobiljonrasulov39@gmail.com>
2024-10-08 16:14:09 +0500

    userSlice added

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -4,18 +4,7 @@ const userSlice = createSlice({
   name: 'users',
   initialState: [],
   reducers: {
-    addUser: (state, action) => {
-      state.push(action.payload);
-    },
-    deleteUser: (state, action) => {
-      return state.filter(user => user.id !== action.payload);
-    },
-    editUser: (state, action) => {
-      const index = state.findIndex(user => user.id === action.payload.id);
-      if (index !== -1) {
-        state[index] = action.payload;
-      }
-    },
+
   },
 });
 
